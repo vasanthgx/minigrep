dff372b798d69834535101e2857ccdb0ebac80ec
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-08 16:40:22 +0530

    creating a constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
 
     // let (query, file_path) = parse_config(&args);
 
-    let config = parse_config(&args);
+    // let config = parse_config(&args);
+
+    let config = Config::new(&args);
 
 
     println!("Searching for {}", config.query);
@@ -20,15 +22,33 @@ fn main() {
     println!( "With text:\n{}", contents);
 }
 
+
+///////////////////////Grouping configuration values with a Struct///////////////////
+
  struct Config {
     query: String,
     file_path: String,
  }
 
+ ////////////////////Extracting the Argument parser ///////////////////////////
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
 
-    Config { query, file_path }
+//     Config { query, file_path }
+// }
+
+
+
+///////////////////////Creating a construcgtor for Config/////////////////
+
+impl Config {
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path}
+    }
 }
+

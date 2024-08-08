7187e7c5ea47840a529df281e8e6064eedf4b44b
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-08 15:40:37 +0530

    reading a file using 'fs::read_to_string()" function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     let query = &args[1];
-    let file_pathe = &args[2];
+    let file_path = &args[2];
 
     println!("Searching for {}", query);
-    println!("In file {}", file_pathe);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Failed to read file");
+
+    println!( "With text:\n{}", contents);
 }

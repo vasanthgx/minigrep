fae77ba57793e3b4daa072d05e9d353da8c75f10
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-08 16:31:25 +0530

    grouping the args values within a Struct called Config

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,28 @@ fn main() {
     // let query = &args[1];
     // let file_path = &args[2];
 
-    let (query, file_path) = parse_config(&args);
+    // let (query, file_path) = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Failed to read file");
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Failed to read file");
 
     println!( "With text:\n{}", contents);
 }
 
+ struct Config {
+    query: String,
+    file_path: String,
+ }
 
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
-    (query, file_path)
+    Config { query, file_path }
 }
